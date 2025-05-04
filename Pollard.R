6be596897b3dcc6e2e9b6015d3a56a5700d2a714
Jekaterīna Butkeviča <jb22035@edu.lu.lv>
2025-05-05 00:45:48 +0300

    pollard

--- Pollard.R
+++ Pollard.R
@@ -74,6 +74,50 @@ ggsave(filename = file.path("Pollard/indeksi", paste0(izveleta_suga, ".png")),
 
 
 
+# Vai ciklā
+sugas <- unique(TDataset_pilnais$latviskais)
+
+
+for (izveleta_suga in sugas) {
+  
+  # Datu filtrēšana un sagatavošana
+  sugas_indeksi <- TDataset_pilnais %>%
+    filter(latviskais == izveleta_suga) %>%
+    group_by(vieta, trans_kods) %>%
+    summarise(skaits = n(), .groups = "drop")
+  
+  # Ja nav datu, pāriet uz nākamo sugu
+  if (nrow(sugas_indeksi) == 0) next
+  
+  # Aprēķina n uz vietu
+  n_labels <- sugas_indeksi %>%
+    group_by(vieta) %>%
+    summarise(n = n(), y = max(skaits) + 1.5)
+  
+  # Zīmē grafiku
+  index_plot <- ggplot(sugas_indeksi, aes(x = vieta, y = skaits, fill = vieta, color = vieta)) +
+    geom_boxplot(outlier.shape = NA, width = 0.4, alpha = 0.4) +    
+    geom_jitter(width = 0.1, height = 0, alpha = 0.7, size = 2) +
+    stat_summary(fun = mean, geom = "point", shape = 21, size = 5, colour = "black") +
+    geom_text(data = n_labels, aes(x = vieta, y = y, label = paste0("n = ", n)), color = "black", size = 4) +
+    scale_y_continuous(
+      limits = c(0, NA),
+      breaks = seq(0, max(sugas_indeksi$skaits) + 2, by = 1)) +
+    theme_minimal(base_size = 14) +
+    labs(x = "Pētījuma vieta", y = "Pollarda pārpilnības indekss", title = izveleta_suga)
+  
+  # Saglabā attēlu
+  ggsave(filename = file.path("Pollard/indeksi", paste0(gsub("[^a-zA-Z0-9_āčēģīķļņšūžĀČĒĢĪĶĻŅŠŪŽ]", "_", izveleta_suga), ".png")),
+         plot = index_plot,
+         width = 10,
+         height = 6,
+         dpi = 300,
+         bg = "white")
+}
+
+
+
+
 
 
 
@@ -85,10 +129,10 @@ ggsave(filename = file.path("Pollard/indeksi", paste0(izveleta_suga, ".png")),
 # Savā metosika Pollards uzsvēra, kā transekte var būt salīdzināma tikai
 # pati ar sevi, nevis savā starpā. Tādēļ izvēlos fenoloģiju būvēt pēc transekšu
 # indeksu summu (jeb novērojumu skaiti) kartrā uzsk. reizē
-.
+
 
 #Iegūstam visus iespējamos vieta–datums pārus
-vieta_datums <- TDataset_sugas %>%
+vieta_datums <- TDataset_pilnais %>%
   distinct(vieta, datums)
 vieta_datums <- vieta_datums[!is.na(vieta_datums$datums),]
 
@@ -99,14 +143,14 @@ dir.create("Pollard/fenologija", recursive = TRUE)
 
 
 # Visas unikālās sugas
-sugas <- unique(TDataset_sugas$latviskais)
+sugas <- unique(TDataset_pilnais$latviskais)
 
 # Cikls pa visām sugām
-for (suga_x in sugas) {
+for (izveleta_suga in sugas) {
   
   # Filtrēt tikai izvēlētas sugas novērojumus
-  sugas_nov <- TDataset_sugas %>%
-    filter(latviskais == suga_x) %>%
+  sugas_nov <- TDataset_pilnais %>%
+    filter(latviskais == izveleta_suga) %>%
     group_by(vieta, datums) %>%
     summarise(skaits = n(), .groups = "drop")
   
@@ -131,7 +175,7 @@ for (suga_x in sugas) {
       breaks = seq(0, max(suga_dati$skaits), by = 1)
     ) +
     labs(
-      title = paste("", suga_x),
+      title = paste("", izveleta_suga),
       x = "Uzskaites datums",
       y = "Novēroto indivīdu skaits",
       color = "Vieta"
@@ -144,7 +188,7 @@ for (suga_x in sugas) {
     )
   
   # Saglabāšana
-  faila_nosaukums <- gsub("[^a-zA-Z0-9_āčēģīķļņšūžĀČĒĢĪĶĻŅŠŪŽ]", "_", suga_x)
+  faila_nosaukums <- gsub("[^a-zA-Z0-9_āčēģīķļņšūžĀČĒĢĪĶĻŅŠŪŽ]", "_", izveleta_suga)
   faila_cels <- file.path("Pollard/fenologija", paste0(faila_nosaukums, ".png"))
   
   ggsave(filename = faila_cels, plot = p, width = 10, height = 6, dpi = 300, bg = "white")
